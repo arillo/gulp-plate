cd47c5ea67fd948120fc1e016b3e719f81adf554
Nicolas Cusan <nc@arillo.net>
2016-07-21 17:58:56 +0200

    improve some formating

--- gulp/tasks/js-lint.js
+++ gulp/tasks/js-lint.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var config   = require('../config').jslint;
-var coffeelint = require('gulp-coffeelint');
-var stylish = require('jshint-stylish');
+var gulp        = require('gulp');
+var jshint      = require('gulp-jshint');
+var config      = require('../config').jslint;
+var coffeelint  = require('gulp-coffeelint');
+var stylish     = require('jshint-stylish');
 var coffeeStyle = require('coffeelint-stylish');
-var jscs = require('gulp-jscs');
+var jscs        = require('gulp-jscs');
 
 gulp.task('jshint', function(){
   return gulp.src(config.srcJs)
