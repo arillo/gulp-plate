a6e0716cbc8bcd9be7e05bd8d691aae2cd5926b8
aminalhazwani <amin.alhazwani@gmail.com>
2016-10-19 21:24:15 +0200

    [add] glob for json concatenation

--- gulpfile.js/tasks/html.js
+++ gulpfile.js/tasks/html.js
@@ -1,17 +1,26 @@
 'use strict';
 
 var config       = require('../config').html;
-var browserSync  = require('browser-sync')
-var data         = require('gulp-data')
-var gulp         = require('gulp')
+var browserSync  = require('browser-sync');
+var data         = require('gulp-data');
+var gulp         = require('gulp');
 var handleErrors = require('../util/handleErrors');
-var path         = require('path')
-var render       = require('gulp-nunjucks-render')
-var fs           = require('fs')
+var path         = require('path');
+var render       = require('gulp-nunjucks-render');
+var fs           = require('fs');
+var _            = require('lodash');
+var glob         = require("glob");
 
-var getData = function(file) {
-  var dataPath = path.resolve(config.data)
-  return JSON.parse(fs.readFileSync(dataPath, 'utf8'))
+var getData = function() {
+  let data = {};
+
+  let files = glob.sync(config.data)
+  files.forEach((el) => {
+    const dataPath = path.resolve(el);
+    _.extend(data, JSON.parse(fs.readFileSync(dataPath, 'utf8')))
+  });
+
+  return data;
 }
 
 var exclude = path.normalize('!**/{' + config.excludeFolders.join(',') + '}/**');
