551d57399edaa30c4d2d652507fc94d1eb321c0c
Nicolas Cusan <nc@arillo.net>
2017-03-13 14:36:12 +0100

    change autoprefioxer settings

--- gulpfile.js/config.js
+++ gulpfile.js/config.js
@@ -7,6 +7,16 @@ const dir   = process.env.PWD;
 const src   = path.resolve(dir, 'src');
 const dest  = path.resolve(dir, 'dist');
 
+// Generic task to move static assets.
+// Files are not watched for changes.
+// All all the paths you need.
+const assets = [
+  // {
+  //   src: `${src}/fonts/**`,
+  //   dest: `${dest}/fonts`,
+  // },
+];
+
 const browserSync = {
   port: 9000,
   server: {
@@ -34,15 +44,6 @@ const images = {
   dest: `${dest}/images`,
 };
 
-// Generic task to move static assets.
-// Files are not watched for changes.
-const assets = [
-  // {
-  //   src: `${src}/fonts/**`,
-  //   dest: `${dest}/fonts`,
-  // },
-];
-
 const sass = {
   src: `${src}/sass/**/*.{sass,scss}`,
   dest: `${dest}/css`,
@@ -54,18 +55,9 @@ const sass = {
       './node_modules/normalize.css',
     ],
   },
-  prefix: [
-    'ie >= 10',
-    'ie_mob >= 10',
-    'ff >= 30',
-    'chrome >= 34',
-    'safari >= 7',
-    'opera >= 28',
-    'ios >= 8',
-    'edge >= 13',
-    'android >= 4.4',
-    'bb >= 10',
-  ],
+
+  prefix: 'last 3 versions',
+
   // Css Selectors that should be removed from your css.
   // useful to remove unneeded thirdparty styles.
   remove: [],
@@ -86,6 +78,7 @@ const sprite = {
 
   // Sprite type: `symbol` or `css` (for bg images)
   type: 'symbol',
+
   sassDest: '../../src/sass/base/_sprite.scss',
   spriteImgName: 'sprite.svg',
   templateSymbol: 'gulpfile.js/tpl/_sprite-symbol.scss',
